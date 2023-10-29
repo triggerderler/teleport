d9e15854fc4620c15a88003a4ad7ed6fccdbcf7b
triggerderler <149014577+triggerderler@users.noreply.github.com>
2023-10-29 13:44:59 +0300

    Update config.lua

--- teleport/config.lua
+++ teleport/config.lua
@@ -4,7 +4,9 @@ cfg = {
 	banka = vector3(314.33, -278.33, 54.17),
 	heading = 161.61,
 	hastane = vector3(306.47, -588.72, 43.28),
-	heading2= 227.01,
+	heading1= 227.01,
 	iskele = vector3(-1801.86, -1207.96, 14.3),
-	heading3 = 225.8
+	heading2 = 225.8,
+	galeri = vector3(-102.84, -1089.78, 26.27),
+	heading3 = 251.48
 }
